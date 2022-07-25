f594aae706d4f9d7f3ddc3ec9882aa4f618f0c76
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2022-07-24 18:40:26 -0600

    Realiza el ejercicio de números en JavaScript de Open Bootcamp

---
+++ index.js
@@ -0,0 +1,22 @@
+//- Una variable que contenga tu altura en centímetros (entero)
+let altura = 178;
+console.log(altura);
+
+//- Una variable que contenga tu altura en metros (número de coma flotante)
+let altura_metros = 1.78;
+console.log(altura_metros);
+
+//- Una variable que contenga tu peso en kilogramos (número de coma flotante)
+let peso = 94.4;
+
+//- Una variable que contenga tu altura en metros redondeada hacia arriba
+let altura_round_up = Math.round(altura_metros);
+console.log(altura_round_up);
+
+//- Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+let altura_round_down = Math.floor(altura_metros);
+console.log(altura_round_down);
+
+//- Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+let max_value = Math.pow(2, 1023) + 1;
+console.log(max_value);
